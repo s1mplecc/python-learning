86dfebe6e5bd0f17c0468f7d2726f2ad1da12b2b
s1mple <s1mple951205@gmail.com>
2020-12-10 14:33:21 +0800

    重载 Vector 类"+ == !="运算符，编写测试用例

--- lecture2/vector.py
+++ lecture2/vector.py
@@ -3,8 +3,25 @@ from functools import reduce
 
 class Vector(object):
     def __init__(self, *args):
-        self._coordinate = args if args else (0,)  # using (0,) when only one item in tuple
+        self.coordinate = args if args else (0,)  # using (0,) when only one item in tuple
 
     def __str__(self) -> str:
-        s = reduce(lambda x, y: str(x) + ', ' + str(y), self._coordinate)
+        s = reduce(lambda x, y: str(x) + ', ' + str(y), self.coordinate)
         return f'Vector({s})'
+
+    def __add__(self, other):
+        if len(self.coordinate) != len(other.coordinate):
+            return None
+        result = [i + other.coordinate[index] for index, i in enumerate(self.coordinate)]
+        return Vector(*result)  # '*' deconstruct the result list
+
+    def __eq__(self, other) -> bool:
+        if len(self.coordinate) != len(other.coordinate):
+            return False
+        for index, i in enumerate(self.coordinate):
+            if i != other.coordinate[index]:
+                return False
+        return True
+
+    def __ne__(self, other) -> bool:
+        return not self.__eq__(other)
--- lecture2/vector_test.py
+++ lecture2/vector_test.py
@@ -10,6 +10,25 @@ class TestVector:
         v2 = Vector(1, 2)
         assert str(v2) == 'Vector(1, 2)'
 
+        v3 = Vector(*(1, 2, 3))
+        assert str(v3) == 'Vector(1, 2, 3)'
+
+    def test_should_compare_two_vectors(self):
+        v1 = Vector(1, 2)
+        v2 = Vector(1, 2)
+        v3 = Vector(2, 3)
+
+        assert v1 is not v2
+        assert v1 == v2
+        assert v1 != v3
+
+    def test_should_add_two_vectors(self):
+        v1 = Vector(1, 2)
+        v2 = Vector(1, 3)
+        result = Vector(2, 5)
+
+        assert result == v1 + v2
+
 
 if __name__ == "__main__":
     pytest.main(['-p', 'no:cacheprovider'])
