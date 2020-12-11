6023a4f3d0e6a4adcc946bae01921249ebe47e7d
s1mple <s1mple951205@gmail.com>
2020-12-11 19:23:22 +0800

    rename tests name using "Given-When-Then" model

--- lecture1/vector_test.py
+++ lecture1/vector_test.py
@@ -25,9 +25,9 @@ class TestVector:
     def test_should_add_two_vectors_with_add_operator(self):
         v1 = Vector(1, 2)
         v2 = Vector(1, 3)
-        result = Vector(2, 5)
+        expected = Vector(2, 5)
 
-        assert result == v1 + v2
+        assert v1 + v2 == expected
 
 
 if __name__ == "__main__":
