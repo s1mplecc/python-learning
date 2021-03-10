58432498a774687e81380bdaec415b1ba3b6b345
s1mple <s1mple951205@gmail.com>
2021-03-10 17:07:38 +0800

    add class Vector test override operators with magic methods

--- lecture2/magicmethods_test.py
+++ lecture2/magicmethods_test.py
@@ -1,3 +1,6 @@
+from vector import Vector
+
+
 class Singleton:
     _instance = None
 
@@ -31,6 +34,15 @@ class Person:
         return cls(name, 'FEMALE')
 
 
+class Incrementor:
+    def __init__(self):
+        self.counter = 0
+
+    def __call__(self):
+        self.counter += 1
+        return self.counter
+
+
 class TestMagicMethods:
     def test_should_new_singleton_instance_with_new_method(self):
         s1 = Singleton()
@@ -41,7 +53,7 @@ class TestMagicMethods:
     def test_should_new_method_run_before_init_method(self):
         assert Employee().name == 'Jack'
 
-    def test_multiple_constructors_with_class_method_and_default_arguments(self):
+    def test_should_multiple_constructors_with_class_method_and_default_arguments(self):
         p1 = Person('Jack')
         p2 = Person('Jane', 'FEMALE')
         p3 = Person.female('Neo')
@@ -51,3 +63,16 @@ class TestMagicMethods:
         assert p2.sex == 'FEMALE' and p2.name == 'Jane'
         assert p3.sex == 'FEMALE' and p3.name == 'Neo'
         assert p4.sex == 'MALE' and p4.name == 'Tony'
+
+    def test_should_increment_with_call_magic_method(self):
+        inc = Incrementor()
+        assert inc() == 1
+        assert inc() == 2
+
+    def test_should_override_operators_with_magic_methods(self):
+        v1 = Vector(1, 2)
+        v2 = Vector(2, 3)
+        v3 = Vector(3, 5)
+
+        assert v1 != v2
+        assert v3 == v1 + v2
--- lecture2/vector.py
+++ lecture2/vector.py
@@ -1,33 +1,16 @@
-import itertools
-from functools import reduce
+class Vector:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
+    def __repr__(self):
+        return f'Vector({self.x}, {self.y})'
 
-class Vector(object):
-    def __init__(self, *args):
-        self.coordinate = args if args else (0,)  # using (0,) when only one item in tuple
-
-    def __str__(self) -> str:
-        s = reduce(lambda x, y: str(x) + ', ' + str(y), self.coordinate)
-        return f'Vector({s})'
-
-    # todo
-    def __iter__(self):
-        return iter(self.coordinate)
-
-    # todo
     def __add__(self, other):
-        pairs = itertools.zip_longest(self, other, fillvalue=0)
-        in_pairs = (a + b for a, b in pairs)
-        print(in_pairs)
-        return Vector(in_pairs)
+        return Vector(self.x + other.x, self.y + other.y)
 
-    def __eq__(self, other) -> bool:
-        if len(self.coordinate) != len(other.coordinate):
-            return False
-        for index, i in enumerate(self.coordinate):
-            if i != other.coordinate[index]:
-                return False
-        return True
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
 
-    def __ne__(self, other) -> bool:
+    def __ne__(self, other):
         return not self.__eq__(other)
