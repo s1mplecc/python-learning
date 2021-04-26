3f90a0bc4d64aa385bdbf38c16678c8270774e81
s1mple <s1mple951205@gmail.com>
2021-04-26 11:43:02 +0800

    modify test slots and dict performance command arguments

--- lecture5/slots.py
+++ lecture5/slots.py
@@ -19,9 +19,9 @@ class PersonWithDict:
 if __name__ == '__main__':
     mem_init = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
     print('Initial RAM Usage: {:12,}'.format(mem_init))
-    if sys.argv[1] == 'slots':
+    if sys.argv[1] == '--use-slots':
         persons = [PersonWithSlots('xxx', 20) for i in range(10 ** 6)]
-    elif sys.argv[1] == 'dict':
+    elif sys.argv[1] == '--use-dict':
         persons = [PersonWithDict('xxx', 20) for i in range(10 ** 6)]
     mem_final = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
     print('  Final RAM Usage: {:12,}'.format(mem_final))
