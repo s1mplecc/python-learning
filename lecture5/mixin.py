fdfb9dffb8fff440f75550e96ba848b73dc24a91
s1mple <s1mple951205@gmail.com>
2021-05-13 10:33:10 +0800

    add mixin class and test

---
+++ lecture5/mixin.py
@@ -0,0 +1,23 @@
+class Dict(dict):
+    def __setitem__(self, key, value):
+        print('2rd be called')
+        super().__setitem__(key, value)
+
+
+class SetOnceMappingMixin:
+    __slots__ = ()
+
+    def __setitem__(self, key, value):
+        if key in self:
+            raise KeyError(str(key) + ' already set')
+        print('1st be called.')
+        return super().__setitem__(key, value)
+
+
+class SetOnceDefaultDict(SetOnceMappingMixin, Dict):
+    ...
+
+
+if __name__ == '__main__':
+    d = SetOnceDefaultDict()
+    d['a'] = 1  # set breakpoint
---
+++ lecture5/multiple_inheritance.py
@@ -0,0 +1,22 @@
+class A:
+    def speak(self):
+        print('class A:', self)
+
+
+class B(A):
+    def speak(self):
+        print('class B:', self)
+
+
+class C(A):
+    def speak(self):
+        print('class C:', self)
+
+
+class D(B, C):
+    pass
+
+
+if __name__ == '__main__':
+    print(D().speak())
+    print(D.__mro__)
